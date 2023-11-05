b5d3803c659b44b2d85491c69d7a66b48b827f1b
Merlin <linmei.merlin@gmail.com>
2023-11-05 18:34:30 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Tansy Tan on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
